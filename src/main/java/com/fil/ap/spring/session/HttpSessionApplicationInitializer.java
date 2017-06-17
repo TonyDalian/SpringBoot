b2b5ab7f91f7b41cc4ae5b899c04a79ce69195b3
sam0411 <sam0411@yeah.net>
2017-06-18 00:49:20 +0800

    fix spring session redis issue - pending

--- src/main/java/com/fil/ap/spring/session/HttpSessionApplicationInitializer.java
+++ src/main/java/com/fil/ap/spring/session/HttpSessionApplicationInitializer.java
@@ -2,10 +2,10 @@ package com.fil.ap.spring.session;
 
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
-public class HttpSessionApplicationInitializer extends AbstractHttpSessionApplicationInitializer { 
+public class HttpSessionApplicationInitializer {//extends AbstractHttpSessionApplicationInitializer { 
 
     public HttpSessionApplicationInitializer() {
     	
-            super(SpringSessionConfig.class); 
+//    	super(SpringSessionConfig.class); 
     }
 }
